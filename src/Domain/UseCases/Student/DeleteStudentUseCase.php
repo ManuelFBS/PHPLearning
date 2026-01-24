d263591228aadd3a7507f2b27c9470e0b84dd3b1
ManuelFBS <manuelf.borrego@gmail.com>
2026-01-24 07:44:26 -0400

    Creación de "DeleteStudentUseCase.php"...

---
+++ src/Domain/UseCases/Student/DeleteStudentUseCase.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Domain\UseCases\Student;
+
+use Domain\Repositories\StudentRepositoryInterface;
+
+/**
+ * ~ Caso de uso: Eliminar un estudiante...
+ * ~ Contiene toda la lógica de negocio para eliminar estudiante...
+ */
+class DeleteProfessorUseCase
+{
+        private StudentRepositoryInterface $studentRepository;
+
+        public function __construct(
+                StudentRepositoryInterface $studentRepository
+        ) {
+                $this->studentRepository = $studentRepository;
+        }
+
+        /**
+         * * Ejecutar el caso de uso de eliminación...
+         *
+         * @param string dni Representa el DNI (Documento Nacional de Identidad) de un estudiante...
+         * @return array ['success' => bool, 'message' => string, 'data' => null]
+         */
+        public function execute(string $dni): array
+        {
+                // > 1. Buscar el profesor por su DNI...
+                $student = $this->studentRepository->findByDni($dni);
+
+                // > 2. Validar si existe el profesor...
+                if ($student === null) {
+                        return [
+                                'success' => false,
+                                'message' => 'Estudiante NO encontrado...',
+                                'data' => null
+                        ];
+                }
+
+                // > 3. Proceder a la eliminación del profesor...
+                $deleteProfessor = $this->professorRepository->delete($dni);
+
+                // > 4. Verificar si la eliminación fue exitosa...
+                if ($deleteProfessor) {
+                        return [
+                                'success' => true,
+                                'message' => 'Profesor eliminado correctamente...',
+                                'data' => null
+                        ];
+                } else {
+                        return [
+                                'success' => false,
+                                'message' => 'NO se pudo eliminar el profesor de la base de datos...',
+                                'data' => null
+                        ];
+                }
+        }
+}
+
+?>
